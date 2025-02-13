c439fe07dda04abbc3cbf6e8f1ced7cff3f2a88b
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-02-13 14:26:19 +0000

    fixes 1

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+// https://vitejs.dev/config/
 
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/TIS-website/",  
 
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  build: {
+    rollupOptions: {
+      external: [], // Ensure everything is bundled
+    }
   }
 })
-
-
